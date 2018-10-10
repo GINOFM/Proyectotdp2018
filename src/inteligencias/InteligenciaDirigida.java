bac80e307cad6d977804fae28ae0081beb440440
Quijano, Martín Nahuel <mnquijano@hotmail.com>
2018-10-10 01:15:02 -0300

    Metodo mover de inteligencia dirigida acomodado para que se pueda esquivar y el enemigo salga de la pantalla para reaparecer.

--- src/inteligencias/InteligenciaDirigida.java
+++ src/inteligencias/InteligenciaDirigida.java
@@ -5,32 +5,23 @@ import entidades.Entity;
 public class InteligenciaDirigida implements Inteligencia {
 
 	private Entity objetivo;
-	
+
 	public InteligenciaDirigida(Entity objetivo) {
-		this.objetivo=objetivo;
+		this.objetivo = objetivo;
 	}
-	
-	public void mover(Entity entidad)
-	{
-		int objetivoX=objetivo.getX();
-		int objetivoY=objetivo.getY();
-		
-		if(objetivoX>entidad.getX())
-		{
-			entidad.setX(entidad.getX()+5);
+
+	public void mover(Entity entidad) {
+		int objetivoX = objetivo.getX();
+
+		if (objetivoX > entidad.getX()) {
+			entidad.setX(entidad.getX() + 4);
 		}
-		if(objetivoX<entidad.getX())
-		{
-			entidad.setX(entidad.getX()-5);
+		if (objetivoX < entidad.getX()) {
+			entidad.setX(entidad.getX() - 4);
 		}
-		if(objetivoY>entidad.getY())
-		{
-			entidad.setY(entidad.getY()+5);
-		}
-		if(objetivoY<entidad.getY())
-		{
-			entidad.setY(entidad.getY()-5);
-		}
+
+		entidad.setY(entidad.getY() + 4);
+
 	}
 
 }
